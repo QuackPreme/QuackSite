fffae180d4887df0395b542443e960917e8e76be
QuackPreme <58330192+QuackPreme@users.noreply.github.com>
2022-03-28 20:48:58 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,28 @@
+function openSM(){
+    document.getElementById("mySidemenu").style.width ="450px";
+    document.getElementById("pg-content").style.marginLeft = "450px";
+    document.getElementById("myAboutmenu").style.width ="450px";
+}
+
+
+
+function closeSM(){
+    document.getElementById("mySidemenu").style.width ="0";
+    document.getElementById("pg-content").style.marginLeft = "0";
+    document.getElementById("myAboutmenu").style.width = "0";
+}
+function openABOUT(){
+    document.getElementById("myAbout").style.width ="100%";
+}
+function openEXPLANATION(){
+    document.getElementById("myExplanation").style.width ="100%";
+}
+function openSPONSOR(){
+    document.getElementById("mySponsor").style.width ="100%";
+}
+function openROBOT(){
+    document.getElementById("myRobot").style.width ="100%";
+}
+function openRESEARCH(){
+    document.getElementById("myResearch").style.width ="100%";
+}
